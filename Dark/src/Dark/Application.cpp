3db8982def6fc1cd83a3b5d1d719e74bd47296ae
YxY <876285983@qq.com>
2020-12-29 23:57:57 +0800

    feat(Dark/src): Window Events

--- Dark/src/Dark/Application.cpp
+++ Dark/src/Dark/Application.cpp
@@ -1,32 +1,47 @@
 #include "dkpch.h"
 #include "Application.h"
-#include "Events/ApplicationEvent.h"
 #include "Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Dark {
-	Application::Application()
-	{
-	  m_Window = std::unique_ptr<Window>(Window::Create());
-	}
 
-	Application::~Application()
-	{
+  #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
-	}
+  Application::Application()
+  {
+    m_Window = std::unique_ptr<Window>(Window::Create());
+	m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
+  }
+  
+  Application::~Application()
+  {
+  
+  }
+  
+  void Application::StartUp()
+  {
+    WindowResizeEvent e(1280, 720);
+    if (e.IsInCategory(EventCategoryApplication))
+    {
+  	while (m_Running)
+  	{
+  	  glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+  	  glClear(GL_COLOR_BUFFER_BIT);
+  	  m_Window->OnUpdate();
+  	}
+    }
+  }
+  void Application::OnEvent(Event & e)
+  {
+	EventDispatcher dispatcher(e);
+	dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 
-	void Application::StartUp()
-	{
-	  WindowResizeEvent e(1280, 720);
-	  if (e.IsInCategory(EventCategoryApplication))
-	  {
-		while (m_Running)
-		{
-		  glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		  glClear(GL_COLOR_BUFFER_BIT);
-		  m_Window->OnUpdate();
-		}
-	  }
-	}
+	DK_CORE_TRACE("{0}", e);
+  }
+  bool Application::OnWindowClose(WindowCloseEvent & e)
+  {
+	m_Running = false;
+    return true;
+  }
 }
--- Dark/src/Dark/Application.h
+++ Dark/src/Dark/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Dark/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Dark {
@@ -13,7 +15,10 @@ namespace Dark {
 		virtual ~Application();
 
 		void StartUp();
+		void OnEvent(Event& e);
 	private:
+	  bool OnWindowClose(WindowCloseEvent& e);
+
 	  std::unique_ptr<Window> m_Window;
 	  bool m_Running = true;
 	};
